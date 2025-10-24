f027bc91dfc47ca9515ce0e27245a7ede07a4993
Andrej Gončarov <gona11@vse.cz>
2025-10-24 13:49:19 +0200

    v1.0.8 přechod na Pages

--- build.sh
+++ build.sh
@@ -1,32 +1,31 @@
-# #!/bin/bash
-# set -euo pipefail
-
-# main () {
-#     HUGO_VERSION=0.151.0
-#     #Instalace Hugo
-#     echo "Probíhá instalace Hugo verze ${HUGO_VERSION}..."
-#     curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
-#     tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
-#     cp hugo /opt/buildhome
-#     rm LICENSE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
-#     #Ověření instalace
-#     echo "Ověření instalace:"
-#     echo Hugo: "$(hugo version)"
-#     #Clone blowfish theme
-#     echo "Klonuji Blowfish theme..."
-#     git submodule update --init --recursive
-#     git config core.quotepath false
-#     #Sestavení
-#     echo "Sestavuji stránku..."
-#     hugo --gc --minify
-# }
-
-# main "$@"
-
 #!/bin/bash
-echo "Inicializuji Git submodules..."
-echo "Nastavuji Blowfish theme..."
-git submodule update --init --recursive
 
-echo "Sestavuji stránku..."
-hugo --gc --minify
+main () {
+    HUGO_VERSION=0.151.0
+    #Instalace Hugo
+    echo "Probíhá instalace Hugo verze ${HUGO_VERSION}..."
+    curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+    tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+    cp hugo /opt/buildhome
+    rm LICENSE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+    #Ověření instalace
+    echo "Ověření instalace:"
+    echo Hugo: "$(hugo version)"
+    #Clone blowfish theme
+    echo "Klonuji Blowfish theme..."
+    git submodule update --init --recursive
+    git config core.quotepath false
+    #Sestavení
+    echo "Sestavuji stránku..."
+    hugo --gc --minify
+}
+
+main "$@"
+
+# #!/bin/bash
+# echo "Inicializuji Git submodules..."
+# echo "Nastavuji Blowfish theme..."
+# git submodule update --init --recursive
+
+# echo "Sestavuji stránku..."
+# hugo --gc --minify
